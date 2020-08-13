22cb3e5d90eca94ae5a7510f3b3e4f9bdca9739d
tomikaa87 <tomikaa87@gmail.com>
2020-08-13 19:26:01 +0200

    Remove unnecessary instructions from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,12 @@
-#include "Config.h"
 #include "CoreApplication.h"
 
 #include <Arduino.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiSTA.h>
-#include <FS.h>
-
 static std::unique_ptr<CoreApplication> application;
-
-void ICACHE_RAM_ATTR timer1Isr()
-{
-    if (application) {
-        application->epochTimerIsr();
-    }
-}
-
-void initializeEpochTimer()
-{
-    timer1_isr_init();
-    timer1_attachInterrupt(timer1Isr);
-    timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
-    timer1_write(100);
-}
-
-void initializeSerial()
-{
-    Serial.begin(74880);
-}
-
-void initWiFi(const ApplicationConfig& appConfig)
-{
-    Serial.println("Connecting to WiFi");
-    WiFi.begin(appConfig.wifi.ssid, appConfig.wifi.password);
-}
-
-#include "drivers/SimpleI2C.h"
-#include "drivers/MCP7940N.h"
-
-using rtc = Drivers::MCP7940N;
 
 void setup()
 {
     static ApplicationConfig appConfig;
-
-    initializeEpochTimer();
-    initializeSerial();
-    initWiFi(appConfig);
 
     application.reset(new CoreApplication(appConfig));
 
